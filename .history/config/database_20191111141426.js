46753937b82abdd1ea1ab3f6bfd5f6387c130182
Saikrishna-jetti <saikrishnaj2@gmail.com>
2019-11-11 17:03:25 +0530

    Role based auth

---
+++ .history/config/database_20191111141426.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     // configure the code below with your username, password and mlab database information
+//     //database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
+    
+//     database: 'mongodb://localhost:27017/rbac',    //dev
+//     secret: 'yoursecret'
+//   }
+
+// to start 'npm start'
